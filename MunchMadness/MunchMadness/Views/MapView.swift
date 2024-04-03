e6d39519fd3e8c9dca1f70523b781a557f77a396
TannerM03 <tanner.macpherson3@gmail.com>
2024-04-03 00:17:20 -0400

    Implement SelectLocationView:
    "Select Location" button now takes user to new View with MapKit
    User can search for a city, address, or whatever they want to
    Map relocates to the location the user searched
    Latitude and longitude of the searched location are stores in SelectLocationView
    
    Next:
    Need to get that latitude and longitude and bring it over to FilterView for API call
    Change UI of the Map pinpoint

---
+++ MunchMadness/MunchMadness/Views/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  MunchMadness
+//
+//  Created by Tanner Macpherson on 4/1/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
---
+++ MunchMadness/MunchMadness/Views/SelectLocationView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectLocationView.swift
+//  MunchMadness
+//
+//  Created by Tanner Macpherson on 4/2/24.
+//
+
+import SwiftUI
+
+struct SelectLocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectLocationView()
+}
